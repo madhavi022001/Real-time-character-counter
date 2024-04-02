05e425f21d95c0b3af82473aefe9492d4de3d88c
madhavi <arigelamadhavi234@gmail.com>
2024-04-02 14:41:05 +0530

    first message is done

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var textarea = document.getElementById('text-here');
+var remining = document.getElementById('Remaining-character');
+var totalcharacters = document.getElementById('total-character');
+
+textarea.addEventListener('input',()=> {
+   
+    var text = textarea.value.trim().split(' ').join('');
+    console.log(text)
+    var totals =text.length;
+    if (totals > 100) {
+        textarea.value = text.slice(0,100);
+        totals = 100;
+        
+    } else{
+
+    
+    var remainingCharacters = 100 -totals;
+    
+      remining.innerHTML = remainingCharacters;
+      totalcharacters.innerHTML=totals;
+    }
+})
+
+
+
+
+
