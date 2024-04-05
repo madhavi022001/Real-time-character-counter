0c91f2bb28ebcedfe612af949486aba6b65bc7d4
madhavi <arigelamadhavi234@gmail.com>
2024-04-05 11:31:43 +0530

    second commit is done

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ var totalcharacters = document.getElementById('total-character');
 
 textarea.addEventListener('input',()=> {
    
-    var text = textarea.value.trim().split(' ').join('');
-    console.log(text)
+    var text = textarea.value;
+
     var totals =text.length;
     if (totals > 100) {
         textarea.value = text.slice(0,100);
@@ -16,8 +16,8 @@ textarea.addEventListener('input',()=> {
     
     var remainingCharacters = 100 -totals;
     
-      remining.innerHTML = remainingCharacters;
-      totalcharacters.innerHTML=totals;
+       remining.innerHTML = remainingCharacters;
+       totalcharacters.innerHTML=totals;
     }
 })
 
